e3cc1fae68e39e3e8edd7fa8be706cec041e4eb6
Vaibhav <v071997@gmail.com>
2022-05-11 13:03:06 +0530

    deployed it to github pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import About from "./components/About";
+// import About from "./components/About";
 import { useState } from "react";
 import Alert from "./components/Alert";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light"); //wheather dark mode is enabled or not
@@ -38,25 +38,25 @@ function App() {
     <>
       {/* <Navbar title="TextUtils" aboutText="About Us" /> */}
       {/* <Navbar /> */}
-      <Router>
+      {/* <Router> */}
         <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className="container my-3">
-          <Routes>
-            <Route exact path="/about" element={<About />}></Route>
-            <Route exact
-              path="/"
-              element={
+          {/* <Routes> */}
+            {/* <Route exact path="/about" element={<About />}></Route> */}
+            {/* <Route exact */}
+              {/* path="/" */}
+              {/* element={ */}
                 <TextForm
                   showAlert={showAlert}
                   heading="Enter the text to analyze below"
                   mode={mode}
                 />
-              }
-            ></Route>
-          </Routes>
+              {/* // } */}
+            {/* ></Route> */}
+          {/* </Routes> */}
         </div>
-      </Router>
+      {/* </Router> */}
     </>
   );
 }
