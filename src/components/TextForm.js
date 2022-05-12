734a918c4bb53724e6de7ac828b2bb1cb6aa9301
Vaibhav <v071997@gmail.com>
2022-05-12 18:54:15 +0530

    Hot fix

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -25,6 +25,7 @@ export default function TextForm(props) {
     let text = document.getElementById("myBox");
     text.select();
     navigator.clipboard.writeText(text.value);
+    document.getSelection().removeAllRanges();
     props.showAlert("Text copied to Clipboard", "success");
   };
 
